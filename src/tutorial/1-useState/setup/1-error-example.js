a6d6a37599f0b0c7d944f6fa84ef74d0a919bc84
David Wu <david204code@gmail.com>
2020-12-17 22:37:55 +0000

    Need to keep the value between render and trigger the re render

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = "Lord Jesus Christ";
+  const handleClick = () => {
+    title = "Son of the livng GOD";
+    console.log(title);
+  };
+
+  return <React.Fragment>
+    <h2>{title}</h2>
+    <button type ="button" className ="btn" onClick ={handleClick}>
+      Change title
+    </button>
+  </React.Fragment>
 };
 
 export default ErrorExample;
