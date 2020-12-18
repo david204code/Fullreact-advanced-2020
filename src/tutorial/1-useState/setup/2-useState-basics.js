fbc52bff4a2346f6269a858ad99df4d9c0865566
David Wu <david204code@gmail.com>
2020-12-18 18:07:53 +0000

    cover the useState basic

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,31 @@
 import React, { useState } from 'react';
+// useState is a function coming from react (named import so name must be exact)
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('hello world'));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+
+  // useState(DEFAULT VALUE) pass invoke pass in default value
+  const [text, setText] = useState('random title')
+
+  const handleClick = () => {
+    if (text === 'random title') {
+      setText('Jesus Christ Son of the Living God!');
+    } else {
+      setText('random title');
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className ="btn" onClick ={handleClick}>
+        Click
+      </button>
+    </React.Fragment>
+  )
 };
 
 export default UseStateBasics;
