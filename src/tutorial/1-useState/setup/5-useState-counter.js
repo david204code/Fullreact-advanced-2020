c230ec203e4c5ad5e7ec269f742820700dc26f8b
David Wu <david204code@gmail.com>
2020-12-21 21:14:41 +0000

    Set up a simple Counter with inline function and reference function example

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+  
+  const reset = () => {
+    setValue(0)
+  }
+
+  return <>
+    <section style ={{margin: '4rem 0'}}>
+      <h2>Regular Counter</h2>
+      <h1>{value}</h1>
+      {/* inline function to decrease the count*/}
+      <button className ="btn" onClick ={() => setValue(value - 1)}>Decrease</button>
+      {/* a reference function */}
+      <button className ="btn" onClick ={reset}>Reset</button>
+      {/* inline function */}
+      <button className ="btn" onClick ={() => setValue(value + 1)}>Increase</button>
+    </section>
+  </>
 };
 
 export default UseStateCounter;
