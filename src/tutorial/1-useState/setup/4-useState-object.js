a125e19f8b7a2f6f8ee1406e2ab261a863b77b7e
David Wu <david204code@gmail.com>
2020-12-21 20:32:40 +0000

    useState object with spread operator which change the selected attribute which leaving the rest as there are

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({name: 'peter', age: 24, message: 'A cheerful heat is good medince'});
+  // console.log(person);
+
+  const changeMessage = () => {
+    // spread operator which leaves the other attribute of the OBJECT as there are
+    setPerson({...person, message: "That's the message to you"});
+  };
+
+  return (
+    <React.Fragment>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className ="btn" onClick ={changeMessage}>
+        Change message
+      </button>
+    </React.Fragment>
+  )
 };
 
 export default UseStateObject;
