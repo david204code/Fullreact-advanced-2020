dfb86c91c1d5262e518171a04cdee5c55cc2479f
David Wu <david204code@gmail.com>
2020-12-21 19:47:36 +0000

    useState arrary example to remove item from array AND empty arror function stops rendering on page load

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,30 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+  
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id)
+    // console.log(newPeople)
+    setPeople(newPeople)
+  } 
+  return <>
+  {
+    people.map((person) => {
+      // destructure
+      const {id, name} = person
+      return <div key ={id} className ="item">
+        <h4>{name}</h4>
+        {/* empty arror function stops it running on render */}
+        <button onClick ={() => removeItem(id)}>Remove</button>
+      </div>;
+    })
+  }
+  {/* set up an empty arror function to stop is rendering on page load */}
+  <button className ="btn" onClick={() => setPeople([])}>
+    clear items
+  </button>
+  </>;
 };
 
 export default UseStateArray;
