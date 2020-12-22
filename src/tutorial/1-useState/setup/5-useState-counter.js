6d934cfda119f89b60bf118cdd895696b82cc59e
David Wu <david204code@gmail.com>
2020-12-22 22:15:43 +0000

    functional update example

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -7,6 +7,19 @@ const UseStateCounter = () => {
     setValue(0)
   }
 
+  const complexIncrease = () => {
+    // setTimeout needs to pass in two things callback function and value 
+    setTimeout(() => {
+      // grabing the out value once NOT the current value so one click at a time
+      // setValue(value + 1);
+      
+      // get the current value
+      setValue((prevState) => {
+        return prevState + 1;
+      })
+    }, 2000)
+  }
+
   return <>
     <section style ={{margin: '4rem 0'}}>
       <h2>Regular Counter</h2>
@@ -17,6 +30,14 @@ const UseStateCounter = () => {
       <button className ="btn" onClick ={reset}>Reset</button>
       {/* inline function */}
       <button className ="btn" onClick ={() => setValue(value + 1)}>Increase</button>
+    </section>
+
+    <section style ={{margin: '4rem 0'}}>
+      <h2>More Complex counter</h2>
+      <h1>{value}</h1>
+      <button className ="btn" onClick ={complexIncrease}>
+        Increase Later
+      </button>
     </section>
   </>
 };
