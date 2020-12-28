1961d045603a5d68db43781a30a1f7776714e3f7
David Wu <david204code@gmail.com>
2020-12-28 19:50:37 +0000

    Short circuit operator example

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,22 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+ 
+  const [text, setText] = useState('');
+  const firstValue = text || 'hello world';
+  const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>value: {secondValue}</h1> */}
+      {/* {if(){console.log('hello world')}} */}
+      {/* if text is empty the string after || is rendered */}
+      <h1>{text || 'Lord Jesus'}</h1>
+      {/* if text is true than the string is return */}
+      {text && <h1>Blessing</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;
