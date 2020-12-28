79f3ff135130a70e279fa13c03285c77f9c01453
David Wu <david204code@gmail.com>
2020-12-28 21:11:34 +0000

    Ternary operator example

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -5,18 +5,28 @@ import React, { useState } from 'react';
 const ShortCircuit = () => {
  
   const [text, setText] = useState('');
-  const firstValue = text || 'hello world';
-  const secondValue = text && 'hello world';
+  const [isError, setIsError] = useState(false);
+  // const firstValue = text || 'hello world';
+  // const secondValue = text && 'hello world';
 
   return (
     <>
       {/* <h1>{firstValue}</h1>
       <h1>value: {secondValue}</h1> */}
-      {/* {if(){console.log('hello world')}} */}
+      
       {/* if text is empty the string after || is rendered */}
       <h1>{text || 'Lord Jesus'}</h1>
+      <button className ="btn" onClick ={() => setIsError(!isError)}>toggle error</button>
       {/* if text is true than the string is return */}
-      {text && <h1>Blessing</h1>}
+      {/* {text && <h1>Blessing</h1>} */}
+      {isError && <h1>faith...</h1>}
+      {/* ternary operator if True <p>is rendered*/}
+      {isError ? (
+        <p>error ternary operator</p>
+      ) : ( 
+      // default if it is not true
+        <div><h2>There is no error</h2></div>
+      )}
     </>
   );
 };
