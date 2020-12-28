a12413bd18f16e62c18879697af8f09de2ef1d44
David Wu <david204code@gmail.com>
2020-12-28 15:17:08 +0000

    multiply returns/condition with fetch data using useEffect()

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -4,13 +4,54 @@ const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturns = () => {
   // functions needs a return otherwise it's an undefined error
+  // these are state value
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState('default user');
 
-  const [loading, setLoading] = useState(true);
+  // useEffect to control the condition
+  useEffect(() => {
+    fetch(url)
+    // set up a contingency for 404 response error
+      .then((response) => {
+        if(response.status >= 200 && response.status <= 299) {
+          return response.json()
+        }
+        else {
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(response.statusText);
+        }
+      })
+      .then((user) => {
+        const {login} = user;
+        setUser(login);
+        setIsLoading(false);
+      })
+      .catch(error => console.log(error)); 
+  },
+  // empty array to prevent infinite loop
+  [])
 
-  if(loading) {
-    return <h2>Thank you Lord!</h2>;
+  if(isLoading) {
+    return (
+      <div>
+        <h1>Loading...Thank you Lord!</h1>
+      </div>
+    );
   }
-  return <h2>Praise the Lord!</h2>;
+  if(isError) {
+    return (
+      <div>
+        <h1>Be patience in affliction.</h1>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>{user}</h1>
+    </div>
+  );
 };
 
 export default MultipleReturns;
