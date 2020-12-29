80bf5c438925cb62fc3851ed47d465a0812ba364
David Wu <david204code@gmail.com>
2020-12-29 19:55:21 +0000

    Show hide toggle example with the clean up function for the useEffect

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,38 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+
+  return <>
+    <button className ="btn" onClick ={() => setShow(!show)}>
+      show/hide
+    </button>
+    {/* disply the Item component if show === true */}
+    {show && <Item />}
+  </>;
 };
+
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+  
+  const checkSize = () => {
+    setSize(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', checkSize);
+    // clean up function
+    return () => {
+      window.removeEventListener('resize', checkSize);
+    }
+  }, [])
+
+  return <>
+    <div style ={{marginTop: '2rem'}}>
+      <h1>window</h1>
+      <h2>Size: {size}px</h2>
+    </div>
+  </>
+}
 
 export default ShowHide;
