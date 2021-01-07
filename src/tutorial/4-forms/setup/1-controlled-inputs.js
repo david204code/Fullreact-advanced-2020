9c3dbbfd0c9639dc237b7f694fe4c98a710f7d54
David Wu <david204code@gmail.com>
2021-01-07 21:43:10 +0000

    inline onChange function for the form example

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -23,11 +23,24 @@ const ControlledInputs = () => {
         <form className ="form" onSubmit ={handleSubmit}>
           <div className ="form-control">
             <label htmlFor ="firstName">Name : </label>
-            <input type ="text" id ="firstName" name ="firstName" value ={firstName}/>
+            <input 
+              type ="text" 
+              id ="firstName" 
+              name ="firstName" 
+              value ={firstName}
+               // access the event object like with event.preventDefault
+              onChange ={(e) => setFirstName(e.target.value)}
+            />
           </div>
           <div className ="form-control">
             <label htmlFor ="email"></label>
-            <input type ="text" id ="email" name ="email" value ={email}/>
+            <input 
+              type ="text" 
+              id ="email" 
+              name ="email" 
+              value ={email} 
+              onChange ={(e) => setEmail(e.target.value)}
+            />
           </div>
           {/* can also be inline onClick function, same concept */}
           {/* <button type ="submit" onClick ={handleSubmit}>Add Person</button> */}
