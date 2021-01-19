f1355a305208572e606de4bbe9948ce4a7087c0f
David Wu <david204code@gmail.com>
2021-01-19 23:15:31 +0000

    Refactor for the reducer example

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,28 +1,50 @@
 import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
+
 // reducer function
+const reducer = (state, action) => {
+
+};
+
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: '',
+};
 
 const Index = () => {
   
   const [name, setName] = useState('');
-  const [people, setPeople] = useState(data);
-  const [showModal, setShowModal] = useState(false);
+  // const [people, setPeople] = useState(data);
+  // const [showModal, setShowModal] = useState(false);
+
+  // useReducer exmaple ....state is the value, dispatch is the function
+  const [state, dispatch] = useReducer(reducer, defaultState);   
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   if(name) {
+  //     setShowModal(true);
+  //     setPeople([...people, {id: new Date().getTime().toString(), name }]);
+  //     setName('');
+  //   }
+  //   else {
+  //     setShowModal(true);
+  //   }
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(name) {
-      setShowModal(true);
-      setPeople([...people, {id: new Date().getTime().toString(), name }]);
-      setName('');
-    }
-    else {
-      setShowModal(true);
+    if (name) {
+
+    } else {
+
     }
   };
 
   return <>
-    {showModal && <Modal />}
+    {/* {showModal && <Modal />}
     <form onSubmit ={handleSubmit} className ="form">
       <div className ="">
         <input type ="text" value ={name} onChange ={(e) => setName(e.target.value)}/>
@@ -33,6 +55,25 @@ const Index = () => {
       return <div key ={person.id}>
         <h4>{person.name}</h4>
       </div>
+    })} */}
+
+    {state.isModalOpen && <Modal modalContent ={state.modalContent}/>}
+    <form onSubmit ={handleSubmit} className ="form">
+      <div className ="">
+        <input 
+          type ="text"
+          value ={name}
+          onChange ={(e) => setName(e.target.value)}
+        />
+      </div>
+      <button type ="submit">Add</button>
+    </form>
+    {state.people.map((person) => {
+      return (
+        <div key ={person.id} >
+          <h4>{person.name}</h4>
+        </div>
+      );
     })}
   </>;
 };
