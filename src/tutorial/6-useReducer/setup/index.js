e5cf91d10e1e24a7ad20656a2ec498dca0e3fdd4
David Wu <david204code@gmail.com>
2021-01-20 14:57:11 +0000

    Add item to list using useReducer example

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -4,7 +4,24 @@ import { data } from '../../../data';
 
 // reducer function
 const reducer = (state, action) => {
-
+  // console.log(state, action);
+  if(action.type === 'ADD_ITEM') {
+    const newPeople = [...state.people, action.payload]
+    return {
+      ...state, 
+      people: newPeople, 
+      isModalOpen: true, 
+      modalContent: "item added"
+    }
+  };
+  if(action.type === 'NO_VALUE') {
+    return {
+      ...state,
+      isModalOpen: true,
+      modalContent: "please enter value"
+    }
+  };
+  throw new Error ('no matching action type');
 };
 
 const defaultState = {
@@ -37,9 +54,11 @@ const Index = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
-
+      const newItem = {id: new Date().getTime().toString(), name}
+      dispatch({type: 'ADD_ITEM', payload: newItem});
+      setName('');
     } else {
-
+      dispatch({type: 'NO_VALUE'});
     }
   };
 
