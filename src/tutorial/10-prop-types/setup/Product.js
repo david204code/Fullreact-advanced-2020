b0ad473c89c00c041089d2b8847d7d5d82e82bc3
David Wu <david204code@gmail.com>
2021-01-22 22:39:55 +0000

    props-types, undefined value from API

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({ image, name, price }) => {
+  return <article className='product'>
+    <h4>Single Product</h4>
+    {/* <img src ={image.url} alt ={name} />
+    <h4>{name}</h4>
+    <p>${price}</p> */}
+  </article>;
 };
 
 export default Product;
