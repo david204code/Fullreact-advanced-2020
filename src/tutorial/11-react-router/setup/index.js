188b05fd5c1b6e4287b08d8a887382f3734a96aa
David Wu <david204code@gmail.com>
2021-01-23 20:54:14 +0000

    ReactRouterBasic example, note exact path for the root page

--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -9,8 +9,31 @@ import Error from './Error';
 import Person from './Person';
 // navbar
 import Navbar from './Navbar';
+
 const ReactRouterSetup = () => {
-  return <h2>react router</h2>;
+  return <Router>
+    
+    <Route exact path ="/">
+      <Home />
+    </Route>
+
+    <Route path ="/about">
+      <About />
+    </Route>
+
+    <Route path ="/people">
+      <People />
+    </Route>
+
+    <Route path ="/error">
+      <Error />
+    </Route>
+
+    <Route path ="person">
+      <Person />
+    </Route>
+    
+  </Router>;
 };
 
 export default ReactRouterSetup;
