16562fcbaf1106c91cdb36389608690b0c9b357e
David Wu <david204code@gmail.com>
2021-01-28 16:26:28 +0000

    useCallback hook example

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -8,8 +8,14 @@ const url = 'https://course-api.com/javascript-store-products'
 // every time props or state changes, component re-renders
 
 const Index = () => {
-  const { products } = useFetch(url)
-  const [count, setCount] = useState(0)
+  const { products } = useFetch(url);
+  const [count, setCount] = useState(0);
+  const [cart, setCart] = useState(0);
+
+  // Add to cart function
+  const addToCart = useCallback(() => {
+    setCart(cart + 1);
+  }, [cart]);
 
   return (
     <>
@@ -17,12 +23,15 @@ const Index = () => {
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style ={{marginTop:"3rem"}}>
+        Cart: {cart}
+      </h1>
+      <BigList products={products} addToCart ={addToCart} />
     </>
   )
 }
 
-const BigList = React.memo(({ products }) => {
+const BigList = React.memo(({ products, addToCart }) => {
   useEffect(() => {
     console.count("Jesus is Lord!");
   });
@@ -30,13 +39,18 @@ const BigList = React.memo(({ products }) => {
   return (
     <section className='products'>
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return <SingleProduct 
+          key={product.id} 
+          {...product} 
+          addToCart ={addToCart}
+        >
+        </SingleProduct>;
       })}
     </section>
   );
 });
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields, addToCart }) => {
   useEffect(() => {
     console.count("Amen!");
   });
@@ -50,6 +64,7 @@ const SingleProduct = ({ fields }) => {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick ={addToCart}>Add to chart</button>
     </article>
   )
 }
